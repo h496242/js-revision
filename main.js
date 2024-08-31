d97901356923025b4565397aad80d303909c7ddd
HASNAIN <161185690+h496242@users.noreply.github.com>
2024-08-31 12:52:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// console.log('Hello Saylani');
+
+// data type : Number string Boolean
+var firstName;
+// assigny operator =
+// if the value cost inside the ""its called string
+
+firstName= 'Hasnain';
+var lastName= 'Ali';
+var numb1= 1;
+var numb2= 2;
+
+alert(numb1 + numb2);
+console.log("Hasnain " + firstName + lastName);
+
+console.log(numb1 + numb2);
+console.log(numb1 - "Hasnain");
+
+;
+console.log(numb1++);
+console.log(++numb1);
+
+console.log(numb1++ + numb2++ - --numb1 + ++numb2 - --numb1);
